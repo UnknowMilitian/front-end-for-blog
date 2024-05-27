ef78e1992a6cfb5871066cd52f78ab3985492d8b
UnknowMilitiant <j.umariy4238@gmail.com>
2024-05-27 11:27:39 +0500

    Completed responsive header

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,16 @@ window.addEventListener("scroll", function () {
     header.classList.remove("scrolled");
   }
 });
+
+// Navbar Section
+const bar = document.querySelector(".bar");
+const navMobile = document.querySelector(".nav-mobile");
+
+// Toggle and close navigation menu
+document.addEventListener("click", (event) => {
+  if (bar.contains(event.target)) {
+    navMobile.classList.toggle("show");
+  } else if (!navMobile.contains(event.target)) {
+    navMobile.classList.remove("show");
+  }
+});
